cf1a91922fbc9107796fa1e417ead38f07de40c9
Nikhil Mishrikoti <nikmys@cs.utah.edu>
2012-03-30 16:52:44 -0600

    "Deleted obsolete declarations"

--- examples/cpu_wait/Externals.h
+++ examples/cpu_wait/Externals.h
@@ -1,9 +1,4 @@
-#include "Trap.h"
 #include "Switch_infprev.h"
-
-/* Trap */
-extern int trap_counter;
-extern struct EventHandler trap_handler;
 
 /* Switch_infprev */
 extern DomAllTimes dom_all_times;
