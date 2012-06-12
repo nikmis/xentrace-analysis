4e2b7201c594ced4c5a0fc73692209882a5de4eb
Nikhil Mishrikoti <nikmys@cs.utah.edu>
2012-06-12 13:25:59 -0600

    Added Event Handler code for exit_to_guest event.
    [ADD]: New struct to store event data.
    [ADD]: Init method to init list and memory.
    [ADD]: Handler code to process and store event data
    	with multi-core support.
    [ADD]: Finalize method that prints total time spent in xen.
    [ADD]: Reset method to clear data during lost_records.
    [ADD]: Accessor method to return last seen exit_to_guest event.

--- examples/xen_dom_time_sharing/Exit_to_guest.c
+++ examples/xen_dom_time_sharing/Exit_to_guest.c
@@ -6,27 +6,114 @@
 #include "EventHandler.h"
 #include "Macros.h"
 #include "Trace.h"
+#include "list.h"
+#include "Exit_to_xen.h"
 #include "Exit_to_guest.h"
 
+XenRuntime xenRuntime;
 
 int exit_to_guest_init(EventHandler *handler)
 {
+	xenRuntime.runtime = 0;
+	xenRuntime.cpuId = 0;
+	
+	memset(xenRuntime.exitToGuest, 0, sizeof(unsigned long long)*MAX_DOMS);
 
+	INIT_LIST_HEAD(&(xenRuntime.cpuList));
+
+	return SUCCESS;
 }
 
 int exit_to_guest_handler(EventHandler *handler, Event *event)
 {
+	XenRuntime *tmpXenRuntime;
+	list_head *head = &(xenRuntime.cpuList);
+	unsigned int domId = (event->data[0] == 0x7fff) ? MAX_DOMS - 1: event->data[0];
 
+	list_for_each_entry(tmpXenRuntime, head, cpuList)
+	{
+		if(tmpXenRuntime->cpuId == event->cpu)
+		{
+			tmpXenRuntime->runtime += event->ns - get_last_exit_to_xen(event->cpu);
+			tmpXenRuntime->exitToGuest[domId] = event->ns;
+			return SUCCESS;
+		}
+	}
+
+	/* If here, cpu object doesn't exist or list empty.
+	 * Malloc new object and add to list.
+	 */
+	unsigned long long lastExitToXen = get_last_exit_to_xen(event->cpu);
+
+	if(!lastExitToXen) lastExitToXen = event->ns;
+
+	XenRuntime *newXenRuntime = (XenRuntime *) malloc(sizeof(XenRuntime));	
+
+	newXenRuntime->runtime = event->ns - lastExitToXen;
+	newXenRuntime->cpuId = event->cpu;
+	
+	memset(newXenRuntime->exitToGuest, 0, sizeof(unsigned long long)*MAX_DOMS);
+
+	newXenRuntime->exitToGuest[domId] = event->ns;
+
+	list_add_tail(&(newXenRuntime->cpuList), head);
+
+	return SUCCESS;
 }
 
 int exit_to_guest_finalize(EventHandler *handler)
 {
+	XenRuntime *tmpXenRuntime;
+	list_head *head = &(xenRuntime.cpuList);
+	unsigned long long totalXenRuntime = 0;
 
+	list_for_each_entry(tmpXenRuntime, head, cpuList)
+	{
+		totalXenRuntime += tmpXenRuntime->runtime;
+	}
+
+	printf("Total time spent in Xen: %5.3f (ms)\n", (float)totalXenRuntime/MEGA);
+
+	list_for_each_entry_reverse(tmpXenRuntime, head, cpuList)
+	{
+		free(tmpXenRuntime);
+	}
+
+	return SUCCESS;
 }
 
 void exit_to_guest_reset(void)
 {
+	XenRuntime *tmpXenRuntime;
+	list_head *head = &(xenRuntime.cpuList);
 
+	list_for_each_entry_reverse(tmpXenRuntime, head, cpuList)
+	{
+		free(tmpXenRuntime);
+	}
+
+	xenRuntime.runtime = 0;
+	xenRuntime.cpuId = 0;
+	
+	memset(xenRuntime.exitToGuest, 0, sizeof(unsigned long long)*MAX_DOMS);
+
+	INIT_LIST_HEAD(&(xenRuntime.cpuList));
+}
+
+unsigned long long get_last_exit_to_guest(unsigned int cpuId, unsigned int domId)
+{
+	XenRuntime *tmpXenRuntime;
+	list_head *head = &(xenRuntime.cpuList);
+
+	list_for_each_entry(tmpXenRuntime, head, cpuList)
+	{
+		if(tmpXenRuntime->cpuId == cpuId)
+		{
+			return tmpXenRuntime->exitToGuest[domId];
+		}
+	}
+
+	return 0;
 }
 
 struct EventHandler exitToGuestHandler =
@@ -34,8 +121,7 @@ struct EventHandler exitToGuestHandler =
 	.name = "exit_to_guest",
 	.event_id = TRC_EXIT_TO_GUEST,
 	.init = exit_to_guest_init,
-	.data = (void*)&,
+	.data = (void*)&xenRuntime,
 	.process_event = exit_to_guest_handler,
-	.finalize = exit_to_guest_reset,
+	.finalize = exit_to_guest_finalize,
 };
-
--- examples/xen_dom_time_sharing/Exit_to_guest.h
+++ examples/xen_dom_time_sharing/Exit_to_guest.h
@@ -1,9 +1,21 @@
 #ifndef __HDLR_EXIT_TO_GUEST
 #define __HDLR_EXIT_TO_GUEST
 
+#include "Exit_to_xen.h"
+
+typedef struct XenRuntime
+{
+	unsigned long long 	exitToGuest[MAX_DOMS];
+	unsigned long long 	runtime;
+	unsigned int		cpuId;
+	list_head 		cpuList;
+} XenRuntime;
+
 int exit_to_guest_init(EventHandler *handler);
 int exit_to_guest_handler(EventHandler *handler, Event *event);
 int exit_to_guest_finalize(EventHandler *handler);
 void exit_to_guest_reset(void);
+
+unsigned long long get_last_exit_to_guest(unsigned int cpuId, unsigned int domId);
 
 #endif
