8def93fe8b8b3727bf1e256e2d4710e9f06a26be
Nikhil Mishrikoti <nikmys@cs.utah.edu>
2012-09-11 17:30:53 -0600

    Track & print lost time by cpu
    [ADD]: New funcs and structs to keep track of lost (records,time,occurences)
    	by cpu.

--- examples/blk_io_queues/Lost_records.c
+++ examples/blk_io_queues/Lost_records.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "Event.h"
 #include "EventHandler.h"
 #include "Macros.h"
 #include "Trace.h"
 #include "Lost_records.h"
+/* For reset methods */
 #include "Back_inflight_queue_blocked.h"
 #include "Back_inflight_queue_unblocked.h"
 #include "Back_request_queue_blocked.h"
@@ -16,15 +18,19 @@
 #include "Front_shared_ring_queue_blocked.h"
 #include "Front_shared_ring_queue_unblocked.h"
 
-static unsigned lostRecCount;
-static unsigned long long totalLostRec;
-static unsigned long long totalLostTime;
+unsigned lostRecCount;
+LostRecTime lostRecTime;
 
 int lost_records_init(EventHandler *handler)
 {
+
 	lostRecCount = 0;
-	totalLostRec = 0;
-	totalLostTime = 0;
+	lostRecTime.totalLostRec = 0;
+	lostRecTime.totalLostTime = 0;
+	lostRecTime.cpu = 0;
+
+	INIT_LIST_HEAD(&(lostRecTime.cpuList));
+
 	return 0;
 }
 
@@ -32,15 +38,28 @@ int lost_records_handler(EventHandler *handler, Event *event)
 {
 	/* Lost records received. Clear all handler data */
 	lostRecCount++;
+	update_lrt_cpulist(event->cpu);
 
 	if(event->lastNs)
 	{
-		totalLostTime += (event->ns - event->lastNs);
-		totalLostRec += event->data[0];
-
 		if((event->ns - event->lastNs) > LOST_REC_MAX_TIME)
 		{
-			printf("CPU: %u : Lost records at %llu : Time Lost = %15.3f (ms)\n", event->cpu, event->ns, (float)(event->ns - event->lastNs)/MEGA);
+			LostRecTime *tmpLrt;
+			list_head *head = &(lostRecTime.cpuList);
+
+			list_for_each_entry(tmpLrt, head, cpuList)
+			{
+				if(tmpLrt->cpu == event->cpu)
+				{
+					tmpLrt->totalLostTime += (event->ns - event->lastNs);
+					tmpLrt->totalLostRec += event->data[0];
+					printf("CPU: %u : Lost records at %llu : Time Lost = %15.3f (ms)\n", 
+							event->cpu, event->ns, 
+							(float)(event->ns - event->lastNs)/MEGA);
+
+				}
+				
+			}
 		}
 	}
 	return 0;
@@ -48,11 +67,65 @@ int lost_records_handler(EventHandler *handler, Event *event)
 
 int lost_records_finalize(EventHandler *handler)
 {
-	printf("\nlost_record occurences = %u\n"
-		 "Total lost_records     = %llu\n"
-		 "Total lost time        = %.3f (ms)\n\n", 
-			lostRecCount, totalLostRec, (float)totalLostTime/MEGA);
+	printf("\nlost_record occurences = %u\n", lostRecCount);
+
+	LostRecTime *tmpLrt;
+
+	list_head *head = &(lostRecTime.cpuList);
+
+	unsigned long long totalCPULostTime = 0;
+	list_for_each_entry(tmpLrt, head, cpuList)
+	{
+		totalCPULostTime += tmpLrt->totalLostTime;
+		 printf("CPU %u : Total lost_records = %llu : Total lost time = %10.3f(ms)\n",
+			tmpLrt->cpu, tmpLrt->totalLostRec, (float)tmpLrt->totalLostTime/MEGA);
+	}
+			
+	printf("Total lost time  = %10.3f (ms)\n\n", (float)totalCPULostTime/MEGA);
+
+	free_lrt_cpulist();
+
 	return 0;
+}
+
+void update_lrt_cpulist(unsigned int cpu)
+{
+	LostRecTime *tmpLrt;
+
+	list_head *head = &(lostRecTime.cpuList);
+
+	if(!list_empty(head))
+	{
+		list_for_each_entry(tmpLrt, head, cpuList)
+		{
+			if(tmpLrt->cpu == cpu)
+			{
+				return;
+			}
+		}
+	}
+
+	/* cpu not found. update list */
+	LostRecTime *newLrt = (LostRecTime *) malloc(sizeof(LostRecTime));
+
+	newLrt->cpu = cpu;
+	
+	newLrt->totalLostRec = 0;
+	newLrt->totalLostTime = 0;
+
+	list_add_tail(&(newLrt->cpuList), head);
+}
+
+void free_lrt_cpulist()
+{
+	LostRecTime *tmpLostRecTime;
+
+	list_head *head = &(lostRecTime.cpuList);
+
+	list_for_each_entry_reverse(tmpLostRecTime, head, cpuList)
+	{
+		free(tmpLostRecTime);
+	}
 }
 
 struct EventHandler lostRecordsHandler = 
--- examples/blk_io_queues/Lost_records.h
+++ examples/blk_io_queues/Lost_records.h
@@ -1,9 +1,19 @@
 #ifndef __HDLR_LOST_RECORDS
 #define __HDLR_LOST_RECORDS
 
+typedef struct LostRecTime
+{
+	int cpu;
+	list_head cpuList;
+	unsigned long long totalLostTime;
+	unsigned long long totalLostRec;
+} LostRecTime;
 
 int lost_records_init(EventHandler *handler);
 int lost_records_handler(EventHandler *handler, Event *event);
 int lost_records_finalize(EventHandler *handler);
+
+void update_lrt_cpulist(unsigned int cpu);
+void free_lrt_cpulist();
 
 #endif
