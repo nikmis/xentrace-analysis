abcc9cc19b016ec4708ea0aa786d31ec7d44b990
Nikhil Mishrikoti <nikmys@cs.utah.edu>
2013-03-19 13:43:25 -0600

    Updated f() decls; New rules in makefile

--- include/Parse.h
+++ include/Parse.h
@@ -21,7 +21,7 @@ typedef struct MinHeap
 
 	void 		(*init)(struct MinHeap *mheap);
 	void 		(*push)(struct MinHeap *mheap, CpuOffset x);
-	CpuOffset 	(*pop)(struct MinHeap *mheap);	
+	int 		(*pop)(struct MinHeap *mheap, CpuOffset *min);	
 } MinHeap;
 
 
@@ -39,7 +39,7 @@ typedef struct Parse
 	// Methods
 	void 	(*setup)(struct Parse *self, FILE *fp);
 	void	(*init_cpu_offset)(struct Parse *self);
-	Event	(*get_next_event)(struct Parse *self);
+	int 	(*get_next_event)(struct Parse *self, Event *ev);
 } Parse;
 
 // MinHeap methods
@@ -50,15 +50,15 @@ void mh_swap(MinHeap *mheap, int n, int par);
 void mh_bubble_up(MinHeap *mheap, int num);
 void mh_push(MinHeap *mheap, CpuOffset x);
 void mh_bubble_down(MinHeap *mheap, int p);
-CpuOffset mh_pop(MinHeap *mheap);
+int mh_pop(MinHeap *mheap, CpuOffset *min);
 void make_heap(MinHeap *mheap, CpuOffset s[], int n);
 void heap_sort(CpuOffset s[], int n);
 
 // Parse methods
 void parse_setup(Parse *self, FILE *fp);
 void parse_init_cpu_offset(Parse *self);
-Event parse_get_next_event(Parse *self);
+int parse_get_next_event(Parse *self, Event *ev);
 Parse* get_parse();
-int parse_return_next_event(FILE *fp, Event *ev);
+int parse_return_next_event(Event *ev);
 
 #endif
