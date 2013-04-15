c8d950729d96f9ce392d3ceb88a338f0539f6f1b
Nikhil Mishrikoti <nikmys@cs.utah.edu>
2013-04-15 12:59:18 -0600

    split pipe()

--- src/Pipe.c
+++ src/Pipe.c
@@ -1,21 +1,38 @@
 #define SIZE 10
 
+typedef enum StageType = {NORMAL, SPLIT, JOIN};
+
 typedef Event (*StageFunc)(struct Stage *s, Event ev);
 
 typedef struct Stage
 {
 	StageFunc f;
-	StageFunc next[SIZE];
+	StageType nextType;
+	union
+	{
+		StageFunc next;
+		StageFunc list_next[SIZE];
+	};
+
 } Stage;
 
 void pipe(Stage *s1, Stage *s2)
 {
+	s1->nextType = NORMAL;
+
+	memcpy(&s1->next, s2, sizeof(Stage));
+}
+
+void split(Stage *s1, Stage *s2)
+{
+	s1->nextType = SPLIT;
+
 	int i = 0;
-	while((i < SIZE) && (s1->next[i] != NULL))
+	while((i < SIZE) && (s1->list_next[i] != NULL))
 		i++;
 	if(i < SIZE)
 	{
-		memcpy(&s1->next[i], s2, sizeof(Stage));
+		memcpy(&s1->list_next[i], s2, sizeof(Stage));
 	}
 	else
 	{
@@ -31,14 +48,22 @@ Event execute_pipe(Stage *s, Event ev)
 	if(ev.id != INVALID)
 	{
 		tmpev = s->f(s, ev);	
-		
-		int i = 0;
-		// Doesnt account for logical OR and joins
-		while((i < SIZE) && (s->next[i] != NULL))
+
+		switch(s->nextType)
 		{
-			execute_pipe(s->next[i], tmpev);
-		}
-		
+			case NORMAL : execute_pipe(&s->next, tmpev);
+				      break;
+			case SPLIT  : 
+				      {
+					      int i = 0;
+					      // Doesnt account for logical OR and joins
+					      while((i < SIZE) && (s->next[i] != NULL))
+					      {
+						      execute_pipe(s->next[i], tmpev);
+					      }
+				      }
+				      break;
+			case JOIN   : break;
 	}
 
 	return tmpev;
